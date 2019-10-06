d0e7da54eeae689810bce62f7f12b7db4768b784
Jason Walter <jaason@protonmail.ch>
2019-10-06 17:35:47 -0400

    Added handlers for half-closed sockets and SIGINT. Still some issues with buffered errors after interrupts but everything works so eh whatever

--- client.c
+++ client.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <curl/curl.h>
 
 #define BUFSIZE 300000
+#define PINGINT 2
 char buffer[BUFSIZE + 1];
+bool interrupt = false;
 
 size_t lr = 0;  // libcurl thing
 
@@ -18,29 +21,43 @@ void frick(char *msg) {
     exit(1);
 }
 
+void handle_interrupt(int sig_type) {
+    printf("Beginning client shutdown ...\n");
+    interrupt = true;
+}
+
 /* A libcurl callback function that I pilfered from
 a generous donor online */
 size_t filterit(void *ptr, size_t size, size_t nmemb, void *stream) {
-  if ( (lr + size*nmemb) > BUFSIZE ) return BUFSIZE;
-  memcpy(stream+lr, ptr, size*nmemb);
-  lr += size*nmemb;
-  return size*nmemb;
+    if ( (lr + size*nmemb) > BUFSIZE ) return BUFSIZE;
+    memcpy(stream+lr, ptr, size*nmemb);
+    lr += size*nmemb;
+    return size*nmemb;
 }
 
 /* Scrape the CHC website and save content to a buffer;
 This should be implemented asynchronously for compatability
 with client socket functions */
 void scrape(void *sockish) {
+    char *token = malloc(BUFSIZE + 1);
+    bool state;
+    int i;
 
     for(;;) {
+        /* Check for interrupt flag and then clean up */
+        if(interrupt == true) {
+            free(token);
+            break;
+        }
+
         /* LOAD HTML FILE FOR TESTING PURPOSES
         DELETE FOR FINAL COMMIT */
         FILE *fp = fopen("snowday.html", "r");
         if (fp != NULL) {
-        int newLen = fread(buffer, sizeof(char), BUFSIZE, fp);
-        if(ferror(fp) != 0) fputs("Error reading file", stderr);
-        else buffer[newLen++] = '\0'; // Just to be safe.
-        fclose(fp);
+            int newLen = fread(buffer, sizeof(char), BUFSIZE, fp);
+            if(ferror(fp) != 0) fputs("Error reading file", stderr);
+            else buffer[newLen++] = '\0'; // Just to be safe.
+            fclose(fp);
         }
 
         /* This is libcurl stuff; set an easy handle / options, and then scrape */
@@ -55,54 +72,58 @@ void scrape(void *sockish) {
 
         /* Awesome parsing algorithm I made that returns the content
         of the <div class="message"> tag */
-        bool state = false;
-        int i = 0;
-        char *token = malloc(BUFSIZE + 1);
+        state = false;
+        i = 0;
         strcpy(token, strtok(buffer, "<>"));
         while(token != NULL && state == false) {
-        if(strcmp(token, "div class=\"message\"") == 0) {
-          strcpy(token, strtok(NULL, "<>"));
-          char *etok = token + 1; // edited token; remove new line feed
-          while(*etok == '\x20') etok++;    // further edit to remove spaces
-          puts(etok);
-          if(send(*(int *)sockish, etok, strlen(etok), 0) < 0) frick("Snow day message failed to be sent");
-          puts("Snow day message sent");
-          state = true;
+            if(strcmp(token, "div class=\"message\"") == 0) {
+                strcpy(token, strtok(NULL, "<>"));
+                char *etok = token + 1; // Edited token; remove new line feed
+                while(*etok == '\x20') etok++;    // Further edit to remove spaces
+                puts(etok);
+                sleep(PINGINT);
+                if(send(*(int *)sockish, etok, strlen(etok), 0) < 0) frick("Snow day message failed to be sent");
+                puts("Snow day message sent");
+                state = true;
+            }
+            if(i > 300) {
+                puts("There's no snow day idiot");
+                strcpy(token, "There's no snow day idiot");
+                sleep(PINGINT);
+                if(send(*(int *)sockish, token, strlen(token), 0) < 0) frick("Snow day message failed to be sent");
+                state = true;
+            }
+            strcpy(token, strtok(NULL, "<>"));
+            i++;
         }
-        if(i > 300) {
-          puts("There's no snow day idiot");
-          strcpy(token, "There's no snow day idiot");
-          if(send(*(int *)sockish, token, strlen(token), 0) < 0) frick("Snow day message failed to be sent");
-          state = true;
-        }
-        strcpy(token, strtok(NULL, "<>"));
-        i++;
-        }
-
-        free(token);  // still need to free the original pointer
-        sleep(2);
     }
 }
 
 int main(int argc, char *argv[]) {
+    if(argc != 2) frick("give me one argument and one argument only!"); // Boilerplate arg check
 
-  if(argc != 2) frick("give me one argument and one argument only!"); // Boilerplate arg check
+    /* Set custom handler for SIGINT */
+    struct sigaction handler;
+    handler.sa_handler = handle_interrupt;
+    if(sigfillset(&handler.sa_mask) < 0) frick("Failed to set signal masks");
+    handler.sa_flags = 0;   // No sa_flags
+    if(sigaction(SIGINT, &handler, 0) < 0) frick("Failed to set the new handler for SIGINT");
 
-  /* Socket creation and server addressing */
-  int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if(sock < 0) frick("Socket creation failed");
+    /* Socket creation and server addressing */
+    int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sock < 0) frick("Socket creation failed");
 
-  struct sockaddr_in server;
-  memset(&server, 0, sizeof server);  // Zero out addressing to avoid ~~bad voodoo~~
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = inet_addr(argv[1]);
-  server.sin_port = htons(8081);
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof server);  // Zero out addressing to avoid ~~bad voodoo~~
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+    server.sin_port = htons(8081);
 
-  /* Establish a connection to the server */
-  if(connect(sock, (struct sockaddr *)&server, sizeof server) < 0) frick("Connection to the server failed or was refused");
-  puts("Connection to the server established");
+    /* Establish a connection to the server */
+    if(connect(sock, (struct sockaddr *)&server, sizeof server) < 0) frick("Connection to the server failed or was refused");
+    puts("Connection to the server established");
 
-  scrape(&sock);
-  close(sock);
-  exit(0);
+    scrape(&sock);
+    close(sock);
+    exit(0);
 }
--- server.c
+++ server.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 
+#define PINGINT 2
+bool interrupt = false;
+unsigned int thr_cnt = 0;   // Thread count
+
+/* Thread arguments structure; allows for future functionality expansion */
 struct thread_args {
     int csock;
 };
@@ -16,6 +23,12 @@ void frick(char *msg) {
     exit(1);
 }
 
+void handle_interrupt(int sig_type) {
+    printf("Beginning server shutdown ...\n");
+    interrupt = true;
+}
+
+/* Modularized client connection function */
 int accept_client(int ssock) {
     int csock;
     struct sockaddr_in caddr;
@@ -27,13 +40,28 @@ int accept_client(int ssock) {
     return csock;
 }
 
+/* Handle data incoming from the client; this runs in an infinite loop per-thread */
 void handle_client(int csock) {
     char msg[512];
     for(;;) {
+        /* Check if client is still alive after ping period */
+        if(recv(csock, msg, 511, MSG_PEEK | MSG_DONTWAIT) == 0) {
+            close(csock);
+            printf("A client disconnected!\n");   // I don't feel like passing the client address through like three functions right now
+            break;
+        }
+        /* Check if SIGINT has been received */
+        if(interrupt == true) {
+            close(csock);
+            break;
+        }
+
+        /* Receive client message */
         int n_bytes = recv(csock, msg, 511, 0);
         msg[n_bytes] = '\0';
         puts(msg);
         /* Handler for snowdays eventually */
+        sleep(PINGINT);   // Wait on ping interval
     }
 }
 
@@ -43,9 +71,9 @@ void *thread_main(void *thrargs) {
     csock = ((struct thread_args *)thrargs) -> csock;
     free(thrargs);
     handle_client(csock);
+    thr_cnt--;
     return NULL;
 }
-
 
 int main(void) {
     int csock, ssock;
@@ -53,29 +81,42 @@ int main(void) {
     pthread_t tid;
     struct thread_args *thrargs;
 
+    /* Set custom handler for SIGINT */
+    struct sigaction handler;
+    handler.sa_handler = handle_interrupt;
+    if(sigfillset(&handler.sa_mask) < 0) frick("Failed to set signal masks");
+    handler.sa_flags = 0;   // No sa_flags
+    if(sigaction(SIGINT, &handler, 0) < 0) frick("Failed to set the new handler for SIGINT");
+
     /* create listening socket */
     if((ssock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) frick("Failed to create socket");
 
-    memset(&saddr, 0, sizeof saddr);
+    /* Set addressing information */
+    memset(&saddr, 0, sizeof saddr);    // Prevents ~~~BAD VOODOO~~~
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
     saddr.sin_port = htons(8081);
 
+    /* Bind! And listen! */
     if(bind(ssock, (struct sockaddr *)&saddr, sizeof saddr) < 0) frick("Failed to bind socket to port");
     if(listen(ssock, 5) < 0) frick("Failed to listen for incoming connections");
 
-    /* begin fun multithreading */
+    /* Begin fun multithreading loop*/
     for(;;) {
-        csock = accept_client(ssock);
-        /* make memory space for client arg */
-        if((thrargs = malloc(sizeof(struct thread_args))) == NULL) frick("Failed to create memory space for new client args");
-        thrargs -> csock = csock;   // Maybe differentiate better between thrargs client socket and the client socket passed to it?
-        /* make client thread */
-        if(pthread_create(&tid, NULL, thread_main, (void *)thrargs) < 0) frick("Failed on pthread_create");
+        if(interrupt == false) {
+            csock = accept_client(ssock);
+            /* Make memory space for client arg */
+            if((thrargs = malloc(sizeof(struct thread_args))) == NULL) frick("Failed to create memory space for new client args");
+            thrargs -> csock = csock;   // Maybe differentiate better between thrargs client socket and the client socket passed to it?
+            /* Make client thread */
+            if(pthread_create(&tid, NULL, thread_main, (void *)thrargs) < 0) frick("Failed on pthread_create");
+            thr_cnt++;
+        }
+        else if (thr_cnt == 0) break;
     }
 
-    /* never reached */
     close(csock);
     close(ssock);
+    free(thrargs);
     exit(0);
 }
